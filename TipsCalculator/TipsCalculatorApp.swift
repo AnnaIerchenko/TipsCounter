9609dfb2160c8b23764f8f60ac45b6f17f6ae884
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2022-01-25 09:08:29 -0800

    Initial Commit

---
+++ TipsCalculator/TipsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsCalculatorApp.swift
+//  TipsCalculator
+//
+//  Created by Ierchenko Anna  on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TipsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
